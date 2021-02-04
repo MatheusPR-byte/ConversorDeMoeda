46fc5a8ad5e087367dd5970a3c8e9f871ac0f43c
Matheus Alves <65434919+MatheusPR-byte@users.noreply.github.com>
2021-02-04 16:12:53 -0300

    Add files via upload

---
+++ ConversorDeMoeda/Calculadora.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConversorDeMoeda {
+    class Calculadora {
+
+        public static double CotacaoDolar;
+        public static double DolaresComprar;
+
+        public static double ConversorDeMoeda(double cotacao, double dolarescomprar) {
+            double porcento = 0.06 * CotacaoDolar * DolaresComprar;
+            return porcento += CotacaoDolar * DolaresComprar;
+
+        }
+
+    }
+}
---
+++ ConversorDeMoeda/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConversorDeMoeda {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Qual é a cotação de dólar? ");
+            Calculadora.CotacaoDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantos dólares você vai comprar? ");
+            Calculadora.DolaresComprar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double TotalReaisTaxaSeis = Calculadora.ConversorDeMoeda(Calculadora.CotacaoDolar, Calculadora.DolaresComprar);
+
+            Console.WriteLine("Valor a ser pago em reais = " + TotalReaisTaxaSeis.ToString("F2", CultureInfo.InvariantCulture));
+
+        }      
+    }
+}
